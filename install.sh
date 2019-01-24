21255eab9576d6506baf7114b6b1813d8819a306
andreaspano <andrea.spano@quantide.com>
2019-01-24 18:22:10 +0100

    tested on azure many things to complete before it runs auto

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
+
+sudo adduser andrea
+sudo usermod -aG sudo andrea
+
 sudo apt-get  update 
 sudo apt-get -y upgrade
 sudo apt-get install -y vim 
@@ -6,6 +10,7 @@ sudo apt-get install -y tmux
 sudo apt-get install -y git 
 sudo apt-get install -y curl
 sudo apt-get install -y neovim
+sudo apt-get install -y sqlite3
 # Latex may yake a while
 sudo apt-get install -y texlive-full
 
@@ -21,10 +26,16 @@ sudo apt-get install -y libxml2-dev
 # https://linuxize.com/post/how-to-install-r-on-ubuntu-18-04/
 # Enable the CRAN repository 
 # TODO automate ubuntu version 
+#ubu_version=$(lsb_release -sc)
+
+# TODO key does not work
+# R install anyway with a warn
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
 
+# add CRAN to source.list
+#r_repo="'deb https://cloud.r-project.org/bin/linux/ubuntu '$ubu_version'-cran35/'"
+#sudo add-apt-repository $repo
 
-# add CRAN to source.list 
 sudo add-apt-repository 'deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/'
 
 
@@ -36,6 +47,7 @@ sudo apt install -y r-base
 sudo apt install -y r-base-dev
 
 # upgrade to R 3.5 ... mah 
+sudo apt-get uodate
 sudo apt-get dist-upgrade
 
 # R packages
@@ -56,6 +68,12 @@ echo "install.packages('tidyverse', lib=\"~/R/x86_64-pc-linux-gnu-library/3.5\" 
 echo "install.packages('roxygen2', lib=\"~/R/x86_64-pc-linux-gnu-library/3.5\"  ,repos=\"https://cran.rstudio.com\")" | R --no-save
 echo "install.packages('forecast', lib=\"~/R/x86_64-pc-linux-gnu-library/3.5\"  ,repos=\"https://cran.rstudio.com\")" | R --no-save
 echo "install.packages('hts', lib=\"~/R/x86_64-pc-linux-gnu-library/3.5\"  ,repos=\"https://cran.rstudio.com\")" | R --no-save
+echo "install.packages('zoo', lib=\"~/R/x86_64-pc-linux-gnu-library/3.5\"  ,repos=\"https://cran.rstudio.com\")" | R --no-save
+echo "install.packages('microbenchmark', lib=\"~/R/x86_64-pc-linux-gnu-library/3.5\"  ,repos=\"https://cran.rstudio.com\")" | R --no-save
+echo "install.packages('testthis', lib=\"~/R/x86_64-pc-linux-gnu-library/3.5\"  ,repos=\"https://cran.rstudio.com\")" | R --no-save
+
+
+
 
 
 echo "devtools::install_github('jalvesaq/colorout')" | R --no-save
@@ -80,7 +98,9 @@ ln -s ~/dev/dots/.vimrc .
 
 
 # R Studio ------------------------
-
+sudo apt-get install gdebi-core
+wget https://download2.rstudio.org/rstudio-server-1.1.463-amd64.deb
+sudo gdebi rstudio-server-1.1.463-amd64.deb
 
 
 # insync --------------------------
@@ -91,4 +111,15 @@ sudo echo "deb http://apt.insynchq.com/ubuntu  bionic non-free contrib" | sudo t
 # install
 sudo apt-get update
 sudo apt-get install insync
+
+
+# data dir
+sudo mkdir /mnt/data
+sudo ln -s /mnt/data /data
+addgroup  dev
+sudo usermod -aG dev  andrea
+sudo usermed -aG dev etabeta
+chgrp -R dev /data
+chmod -R 770 /data
+
 
