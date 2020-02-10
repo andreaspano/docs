3dcceee0e4a90d6a1b602aee336590cdd346929b
andreaspano <andrea.spano@quantide.com>
2020-02-10 16:29:43 +0100

    clone all repos

---
+++ allrepo.sh
@@ -0,0 +1,28 @@
+#!/bin/bash 
+
+ACCESS_TOKEN=9f859da2c391564c998aa67016e94bc9d797a0bc
+ORG_NAME=SDGGroup
+BASE_DIR=/home/andrea/dev
+
+repos=$(curl -u ${ACCESS_TOKEN}:x-oauth-basic -s https://api.github.com/orgs/${ORG_NAME}/repos\?per_page\=200  | grep full_name | cut -f 2 -d":"| tr -d '" ,' )
+
+cd $BASE_DIR
+
+echo "$repos"| while IFS= read -r repo; do 
+  echo "working with" \'$repo\'
+
+  local_dir="${repo//$ORG_NAME\//}"
+
+  if [[ -d "$local_dir" ]]
+  then
+    cd $local_dir
+    echo "pull $local_dir"
+    git pull
+    cd ..
+
+  else
+    echo "clone git@github.com:$repo" in $(pwd)
+    git clone "git@github.com:$repo"
+  fi
+
+done
