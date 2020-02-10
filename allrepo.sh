285c27cb009578888d9a3c2a59e387055b2bcf8f
andreaspano <andrea.spano@quantide.com>
2020-02-10 16:31:18 +0100

    clone all repos

--- allrepo.sh
+++ allrepo.sh
@@ -9,19 +9,19 @@ repos=$(curl -u ${ACCESS_TOKEN}:x-oauth-basic -s https://api.github.com/orgs/${O
 cd $BASE_DIR
 
 echo "$repos"| while IFS= read -r repo; do 
-  echo "working with" \'$repo\'
+  #echo "working with" \'$repo\'
 
   local_dir="${repo//$ORG_NAME\//}"
 
   if [[ -d "$local_dir" ]]
   then
     cd $local_dir
-    echo "pull $local_dir"
+    #echo "Eseguo git pull in $local_dir"
     git pull
     cd ..
 
   else
-    echo "clone git@github.com:$repo" in $(pwd)
+    #echo "Eseguo git clone git@github.com:$repo" in $(pwd)
     git clone "git@github.com:$repo"
   fi
 
